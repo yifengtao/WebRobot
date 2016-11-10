4bb569900ebe1d5c51d9b10c6a44d50177d4b469
yifengtao <taoyf12@sina.com>
2016-11-10 00:12:26 -0500

    ==

--- main.py
+++ main.py
@@ -30,6 +30,28 @@ def upload(count):
     br.set_handle_robots(False)
     br.open('http://zincpharmer.csb.pitt.edu/pharmville/')
     br.form = [form for form in br.forms()][0]
+    form['receptor'] = ['vp40']
+    form.add_file( open(outputBase+str(count)+'.sdf'), 'text/plain', 'test.sdf')
+    form['usrid'] = 'yifengt'
+    form['name'] = 'Yifeng'
+    response = br.submit()
+    # analysis
+    print str(count)+'.sdf'
+    analysis = process()
+    analysis.feed(response.read())
+    analysis.close()
+    br.close()
+
+
+count = 1000
+
+for i in range(847, count + 1, 1):
+    upload(i)
+print '\nAll submitted'
+
+
+
+
 
 
 
