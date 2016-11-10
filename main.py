d70a2183c1ad15e53af5e08ff6e6383e26bcc92b
yifengtao <taoyf12@sina.com>
2016-11-10 01:31:19 -0500

    change form

--- main.py
+++ main.py
@@ -5,37 +5,36 @@ acknowledgement: based on Weiguang Mao's scripts.
 
 import re
 from mechanize import Browser
-from HTMLParser import HTMLParser
-MaxLigand = 1000
+import HTMLParser
 
 pattern = re.compile('Best*')
 
 outputBase = '/Users/yifeng/Documents/Github/localData/'
 
-class process(HTMLParser):
+class process(HTMLParser.HTMLParser):
     html_text = False
     def handle_starttag(self, tag, attr):
         if tag == 'h3':
             self.html_text = True
-        def handle_endtag(self, tag):
-            if tag == 'h3':
-                self.html_text = False
-        def handle_data(self, data):
-            if self.html_text:
-                if pattern.match(data):
-                    print data
+    def handle_endtag(self, tag):
+        if tag == 'h3':
+            self.html_text = False
+    def handle_data(self, data):
+        if self.html_text:
+            if pattern.match(data):
+                print data
 
 def upload(count):
     br = Browser()
     br.set_handle_robots(False)
     br.open('http://zincpharmer.csb.pitt.edu/pharmville/')
-    br.form = [form for form in br.forms()][0]
-    #print br.form
-    print br 
+    form = list(br.forms())[0]
+    br.form = form 
+    
     form['receptor'] = ['traf2']
     form.add_file( open(outputBase+str(count)+'.sdf'), 'text/plain', 'test.sdf')
     form['userid'] = 'yifengt'
-    form['name'] = 'Yifeng'
+    form['name'] = 'Test'
     
     #print form
     response = br.submit()
@@ -48,7 +47,7 @@ def upload(count):
 
 if __name__ == '__main__':
     
-    count = 3
+    count = 1
 
     for i in range(1, count + 1, 1):
         #print i
