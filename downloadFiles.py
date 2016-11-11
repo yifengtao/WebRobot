f2d099a7389a449a3d87d83d6a219a3148ccd2b4
yifengtao <taoyf12@sina.com>
2016-11-10 20:47:30 -0500

    update

--- downloadFiles.py
+++ downloadFiles.py
@@ -24,29 +24,63 @@ class process(HTMLParser.HTMLParser):
             if pattern.match(data):
                 print data
 
-def upload(count):
+def downloadFiles(count):
     br = Browser()
     br.set_handle_robots(False)
     br.open('http://zincpharmer.csb.pitt.edu/pharmer.html')
+    #print response.read()
     #form = list(br.forms())[0]
     #br.form = form 
+
+    # viewer non
+
+    # for control in br.form.controls:
+    #     print control
+    #     print "type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
+
+    formcount=0
+    for form in br.forms():  
+        if str(form.attrs['id']) == 'qfileForm': break
+        formcount=formcount+1
+    br.select_form(nr=formcount)
+    form = br.form
+
+    form.add_file( open(outputBase+'raw/1ca9_lig.sdf'), 'text/plain', 'ligand.sdf')    
+    response = br.submit()
+
+    #print response.read()
+
     formcount=0
     for form in br.forms():  
         if str(form.attrs['id']) == 'receptorForm': break
         formcount=formcount+1
     br.select_form(nr=formcount)
     form = br.form
-    print form
 
-    form.add_file( open(outputBase+str(count)+'.sdf'), 'text/plain', 'receptor.pdb')    
-    #print form
+    form.add_file( open(outputBase+'raw/1ca9_rec.pdb'), 'text/plain', 'receptor.pdb')    
     response = br.submit()
-    # print response.read()
+
+    print response.read()
+    # formcount=0
+    # for form in br.forms():  
+    #     if str(form.attrs['id']) == 'submitButton': break
+    #     formcount=formcount+1
+    # br.select_form(nr=formcount)
+    # form = br.form
+
+    #response = br.submit()
+
+
+
+
+
+
+
     # analysis
-    print str(count)+'.sdf'
-    analysis = process()
-    analysis.feed(response.read())
-    analysis.close()
+    #print str(count)+'.sdf'
+    # analysis = process()
+    # analysis.feed(response.read())
+    # analysis.close()
     br.close()
 
 if __name__ == '__main__':
@@ -55,7 +89,5 @@ if __name__ == '__main__':
 
     for i in range(1, count + 1, 1):
         #print i
-        upload(i)
+        downloadFiles(i)
     print '\nAll submitted'
-
-
