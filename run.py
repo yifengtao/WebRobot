883f8d8752b2368600a86a6348bbc6a536da9f0e
Yifeng Tao <taoyf12@sina.com>
2016-11-17 21:34:36 -0500

    Update run.py

--- run.py
+++ run.py
@@ -2,19 +2,18 @@ from multiprocessing import Pool
 import subprocess
 import glob,os
 
+# function of calculating the sdf from the file fn.
 def f(fn):
     subprocess.call('../smina.static --autobox_ligand ../localData/raw/1ca9_lig.sdf -r ../localData/raw/1ca9_rec.pdb -l inputData/'+fn+'.sdf.gz -o outputData/'+fn+'.sdf --exhaustiveness 1 > outputData/log_'+fn+'.txt', shell=True)
     subprocess.call('../sdsorter.static -sort minimizedAffinity -reduceconfs 1 -nbest 1000 outputData/'+fn+'.sdf refinedData/'+fn+'_refined.sdf', shell=True)
 
+# read into the filenames.
 filename = list()
 os.chdir('inputData')
 for file in glob.glob('*.sdf.gz'):
     filename.append(file[:-7])
 os.chdir('..')
 
-
+# distribute the task to 29 threads.
 pool = Pool(processes=29)
 pool.map(f,filename)
-
-
-
