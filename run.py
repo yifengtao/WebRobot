0fa5716f09b7a3cb0ef32847a913bdf633f5ea0d
yifengtao <taoyf12@sina.com>
2016-11-20 22:32:33 -0500

    debug

--- run.py
+++ run.py
@@ -6,26 +6,25 @@ import random
 # TODO: remember to limit the weight of molecular before docking
 # function of calculating the sdf from the file fn.
 def f(fn):
+    subprocess.call('curl \'ftp://ftp.ncbi.nlm.nih.gov/pubchem/Compound/CURRENT-Full/SDF/'+fn+'.sdf.gz\' -o inputData/'+fn+'.sdf.gz')
     subprocess.call('../smina.static --autobox_ligand ../raw/3BEJ_lig_edit.sdf -r ../raw/3BEJ_REC.pdb -l inputData/'+fn+'.sdf.gz -o outputData/'+fn+'.sdf --exhaustiveness 1 > outputData/log_'+fn+'.txt', shell=True)
-    subprocess.call('../sdsorter.static -sort minimizedAffinity -reduceconfs 1 -nbest 1000 outputData/'+fn+'.sdf refinedData/'+fn+'_refined.sdf', shell=True)
+    subprocess.call('../sdsorter.static -sort minimizedAffinity -reduceconfs 1 -nbest 1000 outputData/'+fn+'.sdf outputData/'+fn+'_refined.sdf', shell=True)
 
 # read into the filenames.
 filename = list()
 fop = open('filename_full.txt', 'r')
 for line in fop:
     filename.append(line.strip())
-for i in filename:
-    print i
+# shuffle filenames
 random.seed(6)
 random.shuffle(filename)
-print '--------------'
-for i in filename:
-    print i
+
+
 #os.chdir('inputData')
 #for file in glob.glob('*.sdf.gz'):
 #    filename.append(file[:-7])
 #os.chdir('..')
 
 # distribute the task to 29 threads.
-#pool = Pool(processes=1)
-#pool.map(f,filename)
+pool = Pool(processes=1)
+pool.map(f,filename)
